f33f4b5faa764fd6ceef4c3cddaa31dbd9f79425
Gilmar <ogilmar@protonmail.com>
2025-03-07 14:31:28 +0100

    data cleaned and summurized data

--- script.R
+++ script.R
@@ -1,7 +1,13 @@
 data(airquality)  # Took for the data cleaning possibility
-head(airquality)
-summary(airquality)
-str(airquality)
 
 library(ggplot2)
 library(dplyr)
+
+summary(airquality)
+is.na(airquality) # Check for missing values
+colSums(is.na(airquality)) # Check for missing values on the original dataset
+
+clean_data <- na.omit(airquality)
+colSums(is.na(clean_data)) # Check for missing values on this cleaned dataset
+
+summary(clean_data)
