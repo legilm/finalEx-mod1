dc584c3e90c6c442498256189d4bdd4048811245
Gilmar <ogilmar@protonmail.com>
2025-03-07 15:10:47 +0100

    2 plots added, and the readme.md

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# finalEx-mod1
+# Air Quality Data Analysis
+
+## Overview
+This project analyzes the `airquality` standard data set in R, exploring relationships between ozone levels, wind speed, temperature, and solar radiation. It includes data cleaning and visualizations.
+
+## Dataset
+- **Source**: Standard R data set `airquality`
+- **Variables**: Ozone, Solar.R, Wind, Temp, Month, Day
+- **Missing Values**: Removed 37 rows with `na.omit()`
+
+## Visualizations
+1. **Ozone vs Wind by Month**  
+   - Scatter plot faceted by month  
+   - Color gradient represents temperature  
+
+2. **Temperature vs Solar Radiation**  
+   - Scatter plot colored by ozone levels  
+   - Shows interaction between temperature and solar radiation  
+
+## Dependencies
+- R
+- Installed packages: ggplot2 and dplyr
+
+##Observations:
+The month of June (06) has the less quantities of data. This could be caused by a common vacation month in the place where the data was gather, or by a maintenance of the equipement that collect the data.
--- script.R
+++ script.R
@@ -5,9 +5,22 @@ library(dplyr)
 
 summary(airquality)
 is.na(airquality) # Check for missing values
-colSums(is.na(airquality)) # Check for missing values on the original dataset
+colSums(is.na(airquality)) # Check for missing values on the original data set
 
 clean_data <- na.omit(airquality)
-colSums(is.na(clean_data)) # Check for missing values on this cleaned dataset
+colSums(is.na(clean_data)) # Check for missing values on this cleaned data set
 
 summary(clean_data)
+
+ggplot(clean_data, aes(x = Wind, y = Ozone)) + #Define the axis
+  geom_point(aes(color = Temp), size = 1.5) + #Set the type of the graph, dot size and set the variable Temp in funtion of the value
+  scale_color_gradient(low = "blue", high = "red") + #Set the color gradient
+  facet_wrap(~Month) +
+  labs(title = "Ozone vs Wind per month", x = "wind speed (mph)", y = "Ozone (ppb)") +
+  theme_minimal()
+
+ggplot(clean_data, aes(x = Temp, y = Solar.R, fill = Ozone)) + #Define the axis, and the fill
+  geom_point(aes(color = Ozone), size = 1.5) + #Set the type of the graph, dot size and set the variable Temp in funtion of the value
+  labs(title = "Relation between temperature, Solar radiation and Ozone", 
+       x = "Temperature (°F)", y = "Solar radiation (W/m²)") + #labels
+  theme_minimal()
